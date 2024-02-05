685e9032f3358a2d3e074bd761f239cf8fc946ec
iarley <7iarley7@gmail.com>
2024-02-05 14:28:26 -0300

    Feat: Iniciando Projeto

---
+++ sql.sql
@@ -0,0 +1,9 @@
+use teste;
+
+create table tab_noticias (
+    id_noticia_tbn INT PRIMARY KEY NOT NULL,
+    nome_noticia_tbn VARCHAR(255),
+    conteudo_noticia_tbn VARCHAR(255)
+);
+
+SELECT * FROM tab_noticias;
