47bfb5c47fcc26a4167216019a34fdba66e62c83
iarley <7iarley7@gmail.com>
2024-02-06 10:02:17 -0300

    Feat: Ajusta autoloader e cabeçalho para requisiçoes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php
 
-require '../vendor/autoload.php';
+require_once('../vendor/autoload.php');
 
-echo "hello";
+date_default_timezone_set("America/Fortaleza");
+
+// definindo o dado esperado
+header("Content-type: application/json");
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: Content-Type");
+
+// echo "hello";
+
+
+use src\Core\Router;
+
+new Router();
