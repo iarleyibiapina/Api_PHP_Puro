196b04f175d3892fcd74a371747fa0086618ce9f
iarley <7iarley7@gmail.com>
2024-02-07 20:03:47 -0300

    Feat: Noticias Model

---
+++ src/Models/Noticias.php
@@ -0,0 +1,61 @@
+<?php
+
+use src\Core\Model;
+
+class Noticias
+{
+    public $id_noticia_tbn;
+    public $nome_noticia_tbn;
+    public $conteudo_noticia_tbn;
+
+    public function index()
+    {
+        $stmt = Model::getConn()->prepate("SELECT * FROM noticias");
+        $results = $stmt->execute();
+
+        if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
+            return $results;
+        } else {
+            return null;
+        }
+    }
+    public function store()
+    {
+        $stmt = Model::getConn()->prepare("INSERT INTO noticias (nome_noticia_tbn, conteudo_noticia_tbn) VALUES (?, ?)");
+        $stmt->bindValue(1, $this->nome_noticia_tbn);
+        $stmt->bindValue(2, $this->conteudo_noticia_tbn);
+        $results = $stmt->execute();
+
+        if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
+            // pega ultimo id inserido (update e delete?)
+            $this->id_noticia_tbn = Model::getConn()->lastInsertId();
+            return $results;
+        } else {
+            print_r($stmt->errorInfo());
+            return null;
+        }
+    }
+    public function view($id_request)
+    {
+        $stmt = Model::getConn()->prepate("SELECT * FROM noticias WHERE id_noticia_tbn = ?");
+        $stmt->bindValue(1, $id_request);
+        $results = $stmt->execute();
+
+        if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
+
+            $this->id_noticia_tbn = $results->id_noticia_tbn;
+            $this->nome_noticia_tbn = $results->nome_noticia_tbn;
+            $this->conteudo_noticia_tbn = $results->conteudo_noticia_tbn;
+
+            return $results;
+        } else {
+            return null;
+        }
+    }
+    public function update($id_request)
+    {
+    }
+    public function delete($id_request)
+    {
+    }
+}
