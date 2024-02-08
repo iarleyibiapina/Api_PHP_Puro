a4dcc1be6f9a2590e8ba2ff432383e3cf442dc59
Iarley Ibiapina Barbosa <iarley.ibiapina@opovodigital.com>
2024-02-08 16:12:26 -0300

    Fix: Altera metodo index e conserta sintaxe pdo

--- src/Models/Noticias.php
+++ src/Models/Noticias.php
@@ -11,11 +11,10 @@ class Noticias
     public $nome_noticia_tbn;
     public $conteudo_noticia_tbn;
 
-    public function index()
+    public function getAll()
     {
-        $stmt = Model::getConn()->prepare("SELECT * FROM tab_noticias");
-        $results = $stmt->execute();
-        if ($results) {
+        $stmt = Model::getConn()->prepare("SELECT * FROM tab_noticias LIMIT 5");
+        if ($stmt->execute()) {
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } else {
             return null;
@@ -26,26 +25,31 @@ class Noticias
         $stmt = Model::getConn()->prepare("INSERT INTO tab_noticias (nome_noticia_tbn, conteudo_noticia_tbn) VALUES (?, ?)");
         $stmt->bindValue(1, $this->nome_noticia_tbn);
         $stmt->bindValue(2, $this->conteudo_noticia_tbn);
-        $results = $stmt->execute();
-
-        if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
-            // pega ultimo id inserido (update e delete?)
-            $this->id_noticia_tbn = Model::getConn()->lastInsertId();
-            return $results;
+        if ($stmt->execute()) {
+            return true;
         } else {
-            print_r($stmt->errorInfo());
-            return null;
+            return false;
         }
+
+        // if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
+        // pega ultimo id inserido (update e delete?)
+        // $this->id_noticia_tbn = Model::getConn()->lastInsertId();
+        // return $results;
+        // return true;
+        // } else {
+        // print_r($stmt->errorInfo());
+        // return null;
+        // }
     }
     public function view($id_request)
     {
-        $stmt = Model::getConn()->prepate("SELECT * FROM tab_noticias WHERE id_noticia_tbn = ?");
+        $stmt = Model::getConn()->prepare("SELECT * FROM tab_noticias WHERE id_noticias_tbn = ?");
         $stmt->bindValue(1, $id_request);
         $results = $stmt->execute();
 
         if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
 
-            $this->id_noticia_tbn = $results->id_noticia_tbn;
+            $this->id_noticia_tbn = $results->id_noticias_tbn;
             $this->nome_noticia_tbn = $results->nome_noticia_tbn;
             $this->conteudo_noticia_tbn = $results->conteudo_noticia_tbn;
 
