e3fd0add6610afa7cf6b93683bfa427e0914b93e
iarley <7iarley7@gmail.com>
2024-02-09 22:29:09 -0300

    Refactor: Removendo funcao getBody para requisicao

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -10,15 +10,4 @@ class Controller
         require_once "../src/Models/" . $model . ".php";
         return new $model;
     }
-
-    protected function getRequestBody()
-    {
-        // pegando dados enviados pelo request
-        $json = file_get_contents("php://input");
-        // esses dados sao pegos em json 
-        $obj = json_decode($json);
-
-        // return $obj;
-        return $_REQUEST;
-    }
 }
