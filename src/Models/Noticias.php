3b9c3c781c34d6e3bf0cb69dc609954303ca6ff2
iarley <7iarley7@gmail.com>
2024-02-09 23:03:10 -0300

    Refactor: Adiciona comentarios as funcoes

--- src/Models/Noticias.php
+++ src/Models/Noticias.php
@@ -11,6 +11,12 @@ class Noticias
     public $nome_noticia_tbn;
     public $conteudo_noticia_tbn;
 
+
+    /**
+     * Retorna todas as noticias
+     *
+     * @return array | null
+     */
     public function getAll()
     {
         $stmt = Model::getConn()->prepare("SELECT * FROM tab_noticias LIMIT 5");
@@ -20,45 +26,52 @@ class Noticias
             return null;
         }
     }
+
+    /**
+     * Armazena noticia, o parametro é enviado pelo controller e pego diretamente pela classe
+     *
+     * @param string $nome_noticia_tbn
+     * @param string $conteudo_noticia_tbn
+     * @return bool
+     */
     public function store()
     {
         $stmt = Model::getConn()->prepare("INSERT INTO tab_noticias (nome_noticia_tbn, conteudo_noticia_tbn) VALUES (?, ?)");
         $stmt->bindValue(1, $this->nome_noticia_tbn);
         $stmt->bindValue(2, $this->conteudo_noticia_tbn);
-        if ($stmt->execute()) {
+        if (!$stmt->execute()) {
             return true;
         } else {
             return false;
         }
-
-        // if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
-        // pega ultimo id inserido (update e delete?)
-        // $this->id_noticia_tbn = Model::getConn()->lastInsertId();
-        // return $results;
-        // return true;
-        // } else {
-        // print_r($stmt->errorInfo());
-        // return null;
-        // }
     }
-    public function view($id_request)
+
+    /**
+     * Retorna uma noticia
+     *
+     * @param int $id_request
+     * @return object | null
+     */
+    public function show($id_request)
     {
         $stmt = Model::getConn()->prepare("SELECT * FROM tab_noticias WHERE id_noticias_tbn = ?");
         $stmt->bindValue(1, $id_request);
         $results = $stmt->execute();
 
         if ($results = $stmt->fetch(PDO::FETCH_OBJ)) {
-
-            // ideia: salva resultado na classe para futuras consultas direto na classe;
-            // $this->id_noticia_tbn = $results->id_noticias_tbn;
-            // $this->nome_noticia_tbn = $results->nome_noticia_tbn;
-            // $this->conteudo_noticia_tbn = $results->conteudo_noticia_tbn;
-
             return $results;
         } else {
             return null;
         }
     }
+
+    /**
+     * Atualiza uma noticia, o parametro é enviado pelo controller e pego diretamente pela classe
+     *
+     * @param int $id_request
+     * @param string $request
+     * @return bool 
+     */
     public function update($id_request, $request)
     {
         // $request['nome_noticia_tbn']
@@ -67,13 +80,19 @@ class Noticias
         $stmt->bindValue(1, $request['nome_noticia_tbn']);
         $stmt->bindValue(2, $request['conteudo_noticia_tbn']);
         $stmt->bindValue(3, $id_request);
-
         if ($stmt->execute()) {
             return true;
         } else {
-            return null;
+            return false;
         }
     }
+
+    /**
+     * Deleta uma noticia
+     *
+     * @param int $id_request
+     * @return bool 
+     */
     public function delete($id_request)
     {
         $stmt = Model::getConn()->prepare("DELETE FROM tab_noticias WHERE id_noticias_tbn = ? ");
@@ -81,7 +100,7 @@ class Noticias
         if ($stmt->execute()) {
             return true;
         } else {
-            return null;
+            return false;
         }
     }
 }
